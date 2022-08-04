c7e893e9c81e3d8ed9598d8ea6e387a39cc66a31
muchtara <muchtaragung3@gmail.com>
2022-08-04 09:07:50 +0700

    hehe

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | Examples:    my-controller/index    -> my_controller/index
 |        my-controller/my-method    -> my_controller/my_method
  */
-$route['default_controller'] = 'auth';
+$route['default_controller'] = 'auth/login';
 $route['login'] = 'auth/login';
 $route['register'] = 'auth/register';
 $route['404_override'] = '';
